5bf6ca00c115228edc8a2918ab82f369affc3f15
Sorenusdb7 <Sorenusdb7@gmail.com>
2025-09-17 14:37:32 -0400

    Use JS Try 5
    Add get upload.js path

--- src/index.ts
+++ src/index.ts
@@ -37,6 +37,10 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'))
 });
 
+app.get('/Upload.js', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'Upload.js'))
+});
+
 //POST API REQUEST
 //Receives PDF for processing
 app.post("/api/upload", upload.single("pdf"), (req, res) => {
